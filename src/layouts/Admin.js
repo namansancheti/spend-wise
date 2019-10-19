a12d7abce66185aa02342c176e74792fd24f7a60
Naman Sancheti <naman.sancheti@gmail.com>
2019-10-19 20:41:36 +0530

    Comment Fixed plugin

--- src/layouts/Admin.js
+++ src/layouts/Admin.js
@@ -117,14 +117,14 @@ export default function Admin({ ...rest }) {
         ) : (
           <div className={classes.map}>{switchRoutes}</div>
         )}
-        <FixedPlugin
+        {/* <FixedPlugin
           handleImageClick={handleImageClick}
           handleColorClick={handleColorClick}
           bgColor={color}
           bgImage={image}
           handleFixedClick={handleFixedClick}
           fixedClasses={fixedClasses}
-        />
+        /> */}
       </div>
     </div>
   );
