dd2c93ba92060b7beb9cb08e46054b16d241796e
Naman Sancheti <naman.sancheti@gmail.com>
2019-10-20 15:14:07 +0530

    Make grid editable and filterable

--- src/views/TableList/TableList.js
+++ src/views/TableList/TableList.js
@@ -69,6 +69,43 @@ export default function TableList() {
           </CardHeader>
           <CardBody>
             <MaterialTable
+              editable={{
+                onRowAdd: newData =>
+                  new Promise((resolve, reject) => {
+                    setTimeout(() => {
+                      {
+                        // const data = this.state.data;
+                        // data.push(newData);
+                        // this.setState({ data }, () => resolve());
+                      }
+                      resolve();
+                    }, 1000);
+                  }),
+                onRowUpdate: (newData, oldData) =>
+                  new Promise((resolve, reject) => {
+                    setTimeout(() => {
+                      {
+                        // const data = this.state.data;
+                        // const index = data.indexOf(oldData);
+                        // data[index] = newData;
+                        // this.setState({ data }, () => resolve());
+                      }
+                      resolve();
+                    }, 1000);
+                  }),
+                onRowDelete: oldData =>
+                  new Promise((resolve, reject) => {
+                    setTimeout(() => {
+                      {
+                        // let data = this.state.data;
+                        // const index = data.indexOf(oldData);
+                        // data.splice(index, 1);
+                        // this.setState({ data }, () => resolve());
+                      }
+                      resolve();
+                    }, 1000);
+                  })
+              }}
               title=""
               columns={[
                 { title: "Category", field: "categoryName" },
@@ -78,7 +115,8 @@ export default function TableList() {
               ]}
               data={transactions}
               options={{
-                search: true
+                search: true,
+                filtering: true
               }}
             />
             )
